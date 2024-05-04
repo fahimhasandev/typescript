eb92976247445235c2d34309120cb7d6d9a341a9
fahimhasandev <fhasan5@wgu.edu>
2024-05-04 18:30:06 -0500

    Array and tuple data type done

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,31 @@ console.log(age);
 let isPassed: boolean = true;
 console.log(isPassed);
 
+/* Array and tuples */
+// Array
+// String array string []
+let scores: string[] = ['a', 'p', 'p', 'l', 'e'];
+const totalScore = scores.reduce((accumulator, currentValue) => {
+  console.log(currentValue);
+  return accumulator + currentValue;
+}, '');
+
+// 0 + a
+console.log(scores[0]);
+console.log(totalScore);
+
+// number [] --> array
+let names: string[] = ['Alice', 'Bob', 'Charlie'];
+console.log(names);
+
+//Alternative way to declare array
+let scores2: Array<number> = [85, 90, 78];
+console.log(scores2);
+
+let names2: Array<string> = ['Fahim', 'john'];
+console.log(names2);
+
+// tuples - fix array length, then use tuples
+let student: [string, number, boolean] = ['alice', 58, true];
+console.log(student[0], student[1], student[2]);
+
